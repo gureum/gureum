1ae2eb4e8fa101df7a7cf3b02e4af436facc126c
Jeong YunWon <jeong@youknowone.org>
2014-02-19 21:42:54 +0900

    Replace tests to XCTests

--- GureumTests/GureumTests.m
+++ GureumTests/GureumTests.m
@@ -1,61 +1,67 @@
 //
-//  CharmIMTests.m
-//  CharmIMTests
+//  GureumTests.m
+//  GureumTests
 //
-//  Created by youknowone on 11. 8. 31..
-//  Copyright 2011 youknowone.org. All rights reserved.
+//  Created by Jeong YunWon on 2014. 2. 19..
+//  Copyright (c) 2014년 youknowone.org. All rights reserved.
 //
 
-#import "GureumTests.h"
-
+#import <XCTest/XCTest.h>
+#import "CIMCommon.h"
 #import "GureumComposer.h"
+#import "CIMInputController.h"
 
-@implementation CharmIMTests
+@interface GureumTests : XCTestCase
+
+@property(nonatomic,retain) CIMInputController *controller;
+@property(nonatomic,retain) CIMMockClient *client;
+
+@end
+
+
+@implementation GureumTests
 
 - (void)setUp {
     [super setUp];
-
-    self->controller = (id)[[CIMMockInputController alloc] initWithServer:nil delegate:nil client:nil];
-    self->client = [[CIMMockClient alloc] init];
-    // Set-up code here.
+    self.controller = (id)[[CIMMockInputController alloc] initWithServer:nil delegate:nil client:nil];
+    self.client = [[CIMMockClient alloc] init];
 }
 
-- (void)tearDown
-{
-    // Tear-down code here.
-    
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testEvent {
+    CIMMockClient *client = self.client;
     [client clearBuffer];
-    [self->controller setValue:kGureumInputSourceIdentifierHan3Final forTag:kTextServiceInputModePropertyTag client:client];
-    [self->controller inputText:@"m" key:46 modifiers:0 client:client];
-    [self->controller inputText:@"f" key:3 modifiers:0 client:client];
-    [self->controller inputText:@"s" key:1 modifiers:0 client:client];
-    [self->controller inputText:@"k" key:40 modifiers:0 client:client];
-    [self->controller inputText:@"g" key:5 modifiers:0 client:client];
-    [self->controller inputText:@"w" key:13 modifiers:0 client:client];
-    [self->controller inputText:@" " key:36 modifiers:0 client:client];
-    STAssertEqualObjects(@"한글 ", client.buffer, @"buffer: %@", client.buffer);
+    [self.controller setValue:kGureumInputSourceIdentifierHan3Final forTag:kTextServiceInputModePropertyTag client:client];
+    [self.controller inputText:@"m" key:46 modifiers:0 client:client];
+    [self.controller inputText:@"f" key:3 modifiers:0 client:client];
+    [self.controller inputText:@"s" key:1 modifiers:0 client:client];
+    [self.controller inputText:@"k" key:40 modifiers:0 client:client];
+    [self.controller inputText:@"g" key:5 modifiers:0 client:client];
+    [self.controller inputText:@"w" key:13 modifiers:0 client:client];
+    [self.controller inputText:@" " key:36 modifiers:0 client:client];
+    XCTAssertEqualObjects(@"한글 ", self.client.buffer, @"buffer: %@", self.client.buffer);
 
     [client clearBuffer];
-    [self->controller setValue:kGureumInputSourceIdentifierHan3Final forTag:kTextServiceInputModePropertyTag client:client];
-    [self->controller inputText:@"m" key:46 modifiers:0 client:client];
-    [self->controller inputText:@"f" key:3 modifiers:0 client:client];
-    [self->controller inputText:@"s" key:1 modifiers:0 client:client];
-    [self->controller inputText:@"k" key:40 modifiers:0 client:client];
-    [self->controller inputText:@"g" key:5 modifiers:0 client:client];
-    [self->controller inputText:@"w" key:13 modifiers:0 client:client];
-    [self->controller inputText:@"\n" key:36 modifiers:0 client:client];
-    STAssertEqualObjects(@"한글\n", client.buffer, @"buffer: %@", client.buffer);
+    [self.controller setValue:kGureumInputSourceIdentifierHan3Final forTag:kTextServiceInputModePropertyTag client:client];
+    [self.controller inputText:@"m" key:46 modifiers:0 client:client];
+    [self.controller inputText:@"f" key:3 modifiers:0 client:client];
+    [self.controller inputText:@"s" key:1 modifiers:0 client:client];
+    [self.controller inputText:@"k" key:40 modifiers:0 client:client];
+    [self.controller inputText:@"g" key:5 modifiers:0 client:client];
+    [self.controller inputText:@"w" key:13 modifiers:0 client:client];
+    [self.controller inputText:@"\n" key:36 modifiers:0 client:client];
+    XCTAssertEqualObjects(@"한글\n", client.buffer, @"buffer: %@", client.buffer);
 }
 
 - (void)testLayoutChange {
-    [client clearBuffer];
-    [self->controller inputText:@" " key:49 modifiers:131072 client:client];
-    [self->controller inputText:@" " key:49 modifiers:131072 client:client];
-    STAssertEqualObjects(@"", client.buffer, @"buffer: %@", client.buffer);
+    [self.client clearBuffer];
+    [self.controller inputText:@" " key:49 modifiers:131072 client:self.client];
+    [self.controller inputText:@" " key:49 modifiers:131072 client:self.client];
+    XCTAssertEqualObjects(@"", self.client.buffer, @"buffer: %@", self.client.buffer);
 }
 
 @end
