e787228bd7291b3636814869c964a9b1b5957193
Jeong YunWon <jeong@youknowone.org>
2014-10-21 02:02:48 +0900

    빌드 스크립트 workspace 쓰도록 수정

--- OSX/tools/build_product.sh
+++ OSX/tools/build_product.sh
@@ -4,7 +4,7 @@
 rm "build/$CONFIGURATION/$pkgname.pkg" "build/$CONFIGURATION/$pkgname.app.tar.gz"
 rm "build/$CONFIGURATION/*.app/Contents/Info.plist"
 
-xcodebuild -project 'Gureum.xcodeproj' -target 'OSX' -configuration "$CONFIGURATION" && \
+xcodebuild -workspace 'Gureum.xcworkspace' -scheme 'OSX' -configuration "$CONFIGURATION" && \
 cd "build/$CONFIGURATION" && \
 productbuild --product "../../OSX/tools/preinst.plist" --component "$appname" '/Library/Input Methods' --sign "Developer ID Installer: YunWon Jeong" "$pkgname.pkg" && \
 tar -zcf "$pkgname.app.tar.gz" "$appname"
