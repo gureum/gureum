0bb2cc0326be485f9ff746a48b36eed68bf62ae5
Jeong YunWon <jeong@youknowone.org>
2014-11-12 10:30:04 +0900

    check version after building

--- OSX/tools/build_product.sh
+++ OSX/tools/build_product.sh
@@ -2,10 +2,11 @@
 . ready.sh
 
 rm "build/$CONFIGURATION/$pkgname.pkg" "build/$CONFIGURATION/$pkgname.app.tar.gz"
-rm "build/$CONFIGURATION/*.app/Contents/Info.plist"
+rm "build/$CONFIGURATION/Gureum.app/Contents/Info.plist"
 
 xcodebuild -workspace 'Gureum.xcworkspace' -scheme 'OSX' -configuration "$CONFIGURATION" && \
 cd "build/$CONFIGURATION" && \
 productbuild --product "../../OSX/tools/preinst.plist" --component "$appname" '/Library/Input Methods' --sign "Developer ID Installer: YunWon Jeong" "$pkgname.pkg" && \
 tar -zcf "$pkgname.app.tar.gz" "$appname"
 
+cat "Gureum.app/Contents/Info.plist" | grep Copyright
