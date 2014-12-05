626a4c6281b8b31b95ec184c610f1a169e6f84e4
Jeong YunWon <jeong@youknowone.org>
2014-12-05 15:49:51 +0900

    preference test

---
+++ Preferences/Preferences.h
@@ -0,0 +1,15 @@
+//
+//  Preferences.h
+//  Preferences
+//
+//  Created by Jeong YunWon on 2014. 11. 12..
+//  Copyright (c) 2014년 youknowone.org. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface Preferences : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
