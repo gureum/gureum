0656c39c584ccbdcc1a80fa294a38c3ae5c78896
Jeong YunWon <jeong@youknowone.org>
2018-08-13 21:24:39 +0900

    Migrate to swift bridge

--- OSX/HangulComposer.swift
+++ OSX/HangulComposer.swift
@@ -8,10 +8,10 @@
 
 import Foundation
 
-@objc public class HangulComposerBridge: NSObject {
+@objcMembers public class HangulComposerBridge: NSObject {
     unowned let composer: CIMComposerDelegate
 
-    @objc public init(composer: CIMComposerDelegate) {
+    public init(composer: CIMComposerDelegate) {
         self.composer = composer
         super.init()
     }
@@ -19,7 +19,31 @@ import Foundation
     // CIMComposerDelegate
 
     public func inputController(_ controller: CIMInputController!, inputText string: String!, key keyCode: Int, modifiers flags: NSEvent.ModifierFlags, client sender: Any!) -> CIMInputTextProcessResult {
-        return CIMInputTextProcessResult.notProcessed
+        let _self = self.composer as! HangulComposer
+        // libhangul은 backspace를 키로 받지 않고 별도로 처리한다.
+        if (keyCode == kVK_Delete) {
+            return _self.inputContext.backspace() ? CIMInputTextProcessResult.processed : CIMInputTextProcessResult.notProcessed
+        }
+
+        if (keyCode > 50 || keyCode == kVK_Delete || keyCode == kVK_Return || keyCode == kVK_Tab || keyCode == kVK_Space) {
+            //dlog(DEBUG_HANGULCOMPOSER, @" ** ESCAPE from outbound keyCode: %lu", keyCode);
+            return CIMInputTextProcessResult.notProcessedAndNeedsCommit;
+        }
+
+        var string = string!
+        // 한글 입력에서 캡스락 무시
+        if flags.contains(.capsLock) {
+            if !flags.contains(.shift) {
+                string = string.lowercased();
+            }
+        }
+        let handled = _self.inputContext.process(string.first!.unicodeScalars.first!.value)
+        let UCSString = _self.inputContext.commitUCSString;
+        // dassert(UCSString);
+        let recentCommitString = HangulComposer.commitStringByCombinationMode(withUCSString: UCSString)
+        _self.commitString.append(recentCommitString)
+        // dlog(DEBUG_HANGULCOMPOSER, @"HangulComposer -inputText: string %@ (%@ added)", self->_commitString, recentCommitString);
+        return handled ? .processed : .notProcessedAndNeedsCancel;
     }
     
     public var composedString: String!
@@ -42,7 +66,7 @@ import Foundation
     
     public var hasCandidates: Bool = false
     
-    public func inputController(_ controller: CIMInputController!, command string: String!, key keyCode: Int, modifiers flags: NSEvent.ModifierFlags, client sender: Any!) -> CIMInputTextProcessResult {
+    public func inputController(_ controller: CIMInputController!, command string: String!, key keyCode: Int, modifier flags: NSEvent.ModifierFlags, client sender: Any!) -> CIMInputTextProcessResult {
         return CIMInputTextProcessResult.notProcessed
     }
 }
