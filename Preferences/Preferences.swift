4febd8ff65aa9bdeae3f1b3911460c5a62ae6dde
SrcDest <hsh6736@gmail.com>
2018-08-23 22:37:32 +0900

    preference pane 옵션 기능 추가

--- Preferences/Preferences.swift
+++ Preferences/Preferences.swift
@@ -10,11 +10,30 @@ import Foundation
 import PreferencePanes
 import Cocoa
 
+class GureumPreferencePane: NSPreferencePane, NSComboBoxDataSource {
 
-class GureumPreferencePane: NSPreferencePane {
-    // @IBOutlet var _window: UIWindow  documented in NSPreferencePane but not automatically supported by IB
-    @IBOutlet var capslockCheckbox: NSButton!
-    var configuration: GureumConfiguration = GureumConfiguration();
+    var configuration: GureumConfiguration = GureumConfiguration()
+    var gureumPreferencesHangulLayouts = [
+        "org.youknowone.inputmethod.Gureum.han2",
+        "org.youknowone.inputmethod.Gureum.han2classic",
+        "org.youknowone.inputmethod.Gureum.han3final",
+        "org.youknowone.inputmethod.Gureum.han3finalloose",
+        "org.youknowone.inputmethod.Gureum.han390",
+        "org.youknowone.inputmethod.Gureum.han390loose",
+        "org.youknowone.inputmethod.Gureum.han3noshift",
+        "org.youknowone.inputmethod.Gureum.han3classic",
+        "org.youknowone.inputmethod.Gureum.hanroman",
+        "org.youknowone.inputmethod.Gureum.hanahnmatae",
+        "org.youknowone.inputmethod.Gureum.han3-2011",
+        "org.youknowone.inputmethod.Gureum.han3-2011loose",
+        "org.youknowone.inputmethod.Gureum.han3-2012",
+        "org.youknowone.inputmethod.Gureum.han3-2012loose",
+        "org.youknowone.inputmethod.Gureum.han3finalnoshift",
+        "org.youknowone.inputmethod.Gureum.han3-2014",
+        "org.youknowone.inputmethod.Gureum.han3-2015" ]
+    var info = Bundle.main.localizedInfoDictionary
+    var names: NSMutableArray = []
+    var gureumPreferencesHangulLayoutLocalizedNames: NSArray = []
 
     func loadFromData() {
 
@@ -23,7 +42,7 @@ class GureumPreferencePane: NSPreferencePane {
     func saveToData() {
 
     }
-
+    
     @IBAction func openKeyboardPreference(sender: NSControl) {
         let myAppleScript = "reveal anchor \"ShortcutsTab\" of pane id \"com.apple.preference.keyboard\""
         var error: NSDictionary?
@@ -33,5 +52,40 @@ class GureumPreferencePane: NSPreferencePane {
             print("pref event descriptor: \(output.stringValue ?? "nil")")
         }
     }
-
+    
+    @IBAction func optionKeyComboBoxValueChanged(_ sender: NSComboBox) {
+        NSLog("sender item: \(sender.indexOfSelectedItem)")
+        configuration.optionKeyBehavior = sender.indexOfSelectedItem
+    }
+    
+    @IBAction func didTapAutoSaveDefaultInputModeCheckBox(_ sender: NSButton) {
+        configuration.autosaveDefaultInputMode = sender.integerValue
+    }
+    
+    @IBAction func defaultHangulInputModeComboBoxValueChanged(_ sender: NSComboBox) {
+        let index = gureumPreferencesHangulLayoutLocalizedNames.index(of: sender.stringValue)
+        configuration.lastHangulInputMode = gureumPreferencesHangulLayouts[index]
+    }
+    
+    @IBAction func didTapRomanModeByEscapeKey(_ sender: NSButton) {
+        configuration.romanModeByEscapeKey = sender.integerValue
+    }
+    
+    func numberOfItems(in comboBox: NSComboBox) -> Int {
+        return gureumPreferencesHangulLayouts.count
+    }
+    
+    func comboBox(_ comboBox: NSComboBox, indexOfItemWithStringValue string: String) -> Int {
+        for layout in gureumPreferencesHangulLayouts {
+            names.add(info![layout] as Any)
+        }
+        gureumPreferencesHangulLayoutLocalizedNames = names
+        
+        return gureumPreferencesHangulLayoutLocalizedNames.index(of: string)
+    }
+    
+    func comboBox(_ comboBox: NSComboBox, objectValueForItemAt index: Int) -> Any? {
+        return gureumPreferencesHangulLayoutLocalizedNames[index]
+    }
 }
+
