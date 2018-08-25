52ae1e866e1e3010843c0b3536cf27f7aac9bfda
강수진 <rkdthd1234@naver.com>
2018-08-25 15:22:12 +0900

    convert keyboardLayout.swift to swift4.0

--- iOS/iOS-Bridging-Header.h
+++ iOS/iOS-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#include <hangeul/capi.h>
+//#include <hangeul/capi.h>
 #include <FoundationExtension/FoundationExtension.h>
 //#include <UIKitExtension/UIKitExtension.h>
 //#include "NSACommonCrypto.h"
