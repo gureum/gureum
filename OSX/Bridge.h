3da4c63f29f4cf9f61c48e6036a726cb5bc59682
혜원 <hyewon@hyewon-ui-macbook-pro.local>
2018-09-01 21:23:10 +0900

    GureumAppDelegate

--- OSX/Bridge.h
+++ OSX/Bridge.h
@@ -2,6 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 #import <Foundation/Foundation.h>
+#import <FoundationExtension/FoundationExtension.h>
 #import <InputMethodKit/InputMethodKit.h>
 
 #import <Hangul/HGInputContext.h>
