7148f5d82f150a0c6c6b47ac98bd17003479e1dc
혜원 <>
2018-09-03 18:37:47 +0900

    GureumAppDelegate.swift

--- OSX/CIMInputControllerGureum.swift
+++ OSX/CIMInputControllerGureum.swift
@@ -11,18 +11,12 @@ import Cocoa
 
 extension CIMInputController {
     @IBAction func checkRecentVersion(_ sender: Any) {
-        /*
-        let versionInfo = GureumAppDelegate.shared()!.recentVersion
-        if versionInfo == nil {
-            return
-        }
-
-        let recent = versionInfo!["recent"] as? String
-        let current = versionInfo!["current"] as? String
-        let download = versionInfo!["download"] as? String
-        let note = versionInfo!["note"] as? String
-        
-
+        let versionInfo = (NSApp.delegate as! GureumAppDelegate).getRecentVersion()
+        let recent = versionInfo["recent"]
+        let current = versionInfo["current"]
+        let download = versionInfo["download"]
+        let note: String? = versionInfo["note"]
+ 
         if (recent == current) {
             let fmt = "현재 사용하고 있는 구름 입력기 \(current ?? "") 는 최신 버전입니다."
             let alert = NSAlert()
@@ -35,7 +29,7 @@ extension CIMInputController {
             if note?.count != 0 {
                 fmt = fmt + " 업데이트 요약은 '\(note ?? "")' 입니다."
             }
-            if download.count == 0 {
+            if download?.count == 0 {
                 fmt = fmt + " 곧 업데이트 링크가 준비될 예정입니다."
             }
             let alert = NSAlert()
@@ -43,12 +37,12 @@ extension CIMInputController {
             alert.addButton(withTitle: "확인")
             alert.informativeText = fmt
             alert.runModal()
-            if (download.count) > 0 {
+            if (download?.count)! > 0 {
                 if let downloadUrl = URL(string: download ?? "") {
                     NSWorkspace.shared.open(downloadUrl)
                 }
             }
-        }*/
+        }
     }
     
     @IBAction func openWebsite(_ sender:Any) {
