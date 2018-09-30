298e4922ec7f69bcdc68adadae947016bcc39953
JimJeon <jimjeon.me@gmail.com>
2018-09-30 17:58:54 +0900

    Add Bridge

---
+++ GureumTests/Bridge.h
@@ -0,0 +1,12 @@
+//
+//  Bridge.h
+//  OSXTests
+//
+//  Created by Jim Jeon on 16/09/2018.
+//  Copyright © 2018 youknowone.org. All rights reserved.
+//
+
+#import "CIMCommon.h"
+#import "CIMInputController.h"
+
+#import "GureumMockObjects.h"
