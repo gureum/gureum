8a0ba06d9054ed394b17d62d616b1ec14fe99ed3
Jeong YunWon <jeong@youknowone.org>
2019-09-24 22:37:59 +0900

    Manually codesign for install_debug.sh

--- tools/install_debug.sh
+++ tools/install_debug.sh
@@ -13,6 +13,8 @@ if [ ! "${INSTALL_PATH}" ]; then
     echo "something wrong" && exit 255
 fi
 
+/usr/bin/codesign --force --sign - --entitlements "${CONFIGURATION_TEMP_DIR}/OSX.build/Gureum.app.xcent" --timestamp=none "${TARGET_BUILD_DIR}/Gureum.app"
+
 run_with_echo sudo rm -rf "\"${INSTALL_PATH}/${PRODUCT_NAME}.app\""
 run_with_echo sudo cp -R "\"${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app\"" "\"${INSTALL_PATH}/\""
 run_with_echo sudo killall -15 "\"${PRODUCT_NAME}\""
