307a1e6cd6779c65789a53bc570489bcdef557c3
Presto <yoohan95@gmail.com>
2019-09-29 15:50:23 +0900

    CI에 swiftformat 도입하고 HACKING에 문서화 (#613)

--- GureumTests/MockApp.swift
+++ GureumTests/MockApp.swift
@@ -24,7 +24,7 @@ class VirtualApp: NSObject {
         controller.updateComposition()
         return processed
     }
-    
+
     @discardableResult
     func inputText(_ string: String!, key keyCode: KeyCode, modifiers flags: NSEvent.ModifierFlags = NSEvent.ModifierFlags(rawValue: 0)) -> Bool {
         let processed = controller.inputText(string, key: keyCode.rawValue, modifiers: Int(flags.rawValue), client: client)
@@ -45,7 +45,6 @@ class VirtualApp: NSObject {
         }
         return inputText(string, key: keyCode, modifiers: flags)
     }
-        
 }
 
 class ModerateApp: VirtualApp {
@@ -59,50 +58,50 @@ class ModerateApp: VirtualApp {
         return processed
     }
 }
+
 /*
-@implementation TerminalApp
+ @implementation TerminalApp
 
-- (BOOL)inputText:(NSString *)text key:(NSInteger)keyCode modifiers:(NSEventModifierFlags)flags {
-    BOOL processed = NO;
-    if (self.client.hasMarkedText) {
-        processed = [super inputText:text key:keyCode modifiers:flags];
-        if (keyCode == 36) {
-            processed = YES;
-        }
-    }
-    else {
-        if (keyCode == 36) {
-            [self.client insertText:text];
-            processed = YES;
-        } else {
-            processed = [super inputText:text key:keyCode modifiers:flags];
-        }
-    }
-    if (!processed) {
-        [self.client insertText:text replacementRange:self.client.markedRange];
-    }
-    return processed;
-}
+ - (BOOL)inputText:(NSString *)text key:(NSInteger)keyCode modifiers:(NSEventModifierFlags)flags {
+ BOOL processed = NO;
+ if (self.client.hasMarkedText) {
+     processed = [super inputText:text key:keyCode modifiers:flags];
+     if (keyCode == 36) {
+         processed = YES;
+     }
+ }
+ else {
+     if (keyCode == 36) {
+         [self.client insertText:text];
+         processed = YES;
+     } else {
+         processed = [super inputText:text key:keyCode modifiers:flags];
+     }
+ }
+ if (!processed) {
+     [self.client insertText:text replacementRange:self.client.markedRange];
+ }
+ return processed;
+ }
 
-@end
+ @end
 
+ @implementation GreedyApp
 
-@implementation GreedyApp
+ - (BOOL)inputText:(NSString *)text key:(NSInteger)keyCode modifiers:(NSEventModifierFlags)flags {
+ BOOL processed = NO;
+ if (self.client.hasMarkedText) {
+     processed = [super inputText:text key:keyCode modifiers:flags];
+ }
+ else {
+     processed = [super inputText:text key:keyCode modifiers:flags];
+     if (self.client.markedRange.length == 0 || !processed) {
+         // FIXME: Commited string should be removed too.
+         [self.client insertText:text replacementRange:self.client.markedRange];
+     }
+ }
+ return processed;
+ }
 
-- (BOOL)inputText:(NSString *)text key:(NSInteger)keyCode modifiers:(NSEventModifierFlags)flags {
-    BOOL processed = NO;
-    if (self.client.hasMarkedText) {
-        processed = [super inputText:text key:keyCode modifiers:flags];
-    }
-    else {
-        processed = [super inputText:text key:keyCode modifiers:flags];
-        if (self.client.markedRange.length == 0 || !processed) {
-            // FIXME: Commited string should be removed too.
-            [self.client insertText:text replacementRange:self.client.markedRange];
-        }
-    }
-    return processed;
-}
-
-@end
-*/
+ @end
+ */
