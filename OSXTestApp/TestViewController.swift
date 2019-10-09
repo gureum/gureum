c283e79bcde006b8f9ff5c5aa7e6884f7c1f5c3c
Chayoung You <yousbe@gmail.com>
2019-10-10 03:21:00 +0900

    Fix PreferenceViewController.loadView of OSXTestApp
    Preferences.xib and OSXTestApp's storyboard were automatically repaired.

--- OSXTestApp/TestViewController.swift
+++ OSXTestApp/TestViewController.swift
@@ -10,16 +10,23 @@ import Cocoa
 @testable import GureumCore
 
 class PreferenceViewController: NSViewController {
+    private let _isAtLeast10_15 = ProcessInfo().isOperatingSystemAtLeast(OperatingSystemVersion(majorVersion: 10, minorVersion: 15, patchVersion: 0))
+
     override func loadView() {
         let path = Bundle.main.path(forResource: "Preferences", ofType: "prefPane")
         let bundle = NSPrefPaneBundle(path: path)!
         assert(bundle.bundle != nil)
         assert(bundle.bundle.principalClass != nil)
-        let loaded = bundle.instantiatePrefPaneObject()
-        assert(loaded)
-        let pane = bundle.prefPaneObject()!
-        // pane.loadMainView()
-        view = pane.mainView
+        if _isAtLeast10_15 {
+            let pane = NSPreferencePane(bundle: bundle.bundle)
+            pane.loadMainView()
+            view = pane.mainView
+        } else {
+            let loaded = bundle.instantiatePrefPaneObject()
+            assert(loaded)
+            let pane = bundle.prefPaneObject()!
+            view = pane.mainView
+        }
     }
 }
 
