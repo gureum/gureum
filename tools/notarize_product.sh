6976bc2011116a047927eb42f86d3c69ed3f9ebf
Jeong YunWon <jeong@youknowone.org>
2020-07-18 01:04:43 +0900

    print error when notarization failed

--- tools/notarize_product.sh
+++ tools/notarize_product.sh
@@ -14,17 +14,19 @@ ZIP_PATH=$1/${PACKAGE_NAME}.zip
 PKG_PATH=$1/${PACKAGE_NAME}.pkg
 
 if [ ! "$1" ]; then
-    echo "run archive and put archive path as 1st argument"
+    echo "run archive and put archive path as 1st argument" >&2
     exit 1
 fi
 
 if [ ! -e "$1" ]; then
-    echo "unexisting path: $1"
+    echo "unexisting path: $1" >&2
     exit 1
 fi
 
 if [ ! -e "$ZIP_PATH" ] || [ ! -e "$PKG_PATH" ]; then
-    echo "The given path doesn't include .zip or .pkg $ZIP_PATH $PKG_PATH"
+    echo "The given path doesn't include .zip or .pkg" >&2
+    echo "  app: $ZIP_PATH" >&2
+    echo "  pkg: $PKG_PATH" >&2
     exit 1
 fi
 
@@ -34,11 +36,21 @@ echo -n "Apple ID> "
 read -r apple_id
 
 echo "Notarizing app..."
-xcrun altool --notarize-app -t osx --primary-bundle-id org.youknowone.inputmethod.Gureum \
-    -u "$apple_id" -p @keychain:developer.apple.com -itc_provider 9384JEL3M9 -f "${ZIP_PATH}"
+cmd=(xcrun altool --notarize-app -t osx --primary-bundle-id org.youknowone.inputmethod.Gureum \
+    -u "$apple_id" -p @keychain:developer.apple.com -itc_provider 9384JEL3M9 -f "${ZIP_PATH}")
+echo "${cmd[@]}"
+if ! "${cmd[@]}"; then
+    echo "Signing app failed: ${ZIP_PATH}" >&2
+    exit $?
+fi
 
 echo "Notarizing pkg..."
-xcrun altool --notarize-app -t osx --primary-bundle-id org.youknowone.inputmethod.Gureum \
-    -u "$apple_id" -p @keychain:developer.apple.com -f "$PKG_PATH"
+cmd=(xcrun altool --notarize-app -t osx --primary-bundle-id org.youknowone.inputmethod.Gureum \
+    -u "$apple_id" -p @keychain:developer.apple.com -f "$PKG_PATH")
+echo "${cmd[@]}"
+if ! "${cmd[@]}"; then
+    echo "Signing pkg failed: ${PKG_PATH}" >&2
+    exit $?
+fi
 
 mv "$PKG_PATH" ~/Downloads/
