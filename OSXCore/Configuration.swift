fce09a8f3b7aed24c349b854c673609588954f44
Chayoung You <yousbe@gmail.com>
2020-10-29 15:30:55 +0900

    Fix swiftformat warnings

--- OSXCore/Configuration.swift
+++ OSXCore/Configuration.swift
@@ -74,8 +74,8 @@ public class Configuration: UserDefaults {
 
     class func convertConfigurationToShortcut(_ configuration: [String: Any]) -> Shortcut? {
         guard let modifier = configuration["modifier"] as? UInt,
-            let keyCodeRawValue = configuration["keyCode"] as? Int,
-            let keyCode = KeyCode(rawValue: keyCodeRawValue)
+              let keyCodeRawValue = configuration["keyCode"] as? Int,
+              let keyCode = KeyCode(rawValue: keyCodeRawValue)
         else {
             return nil
         }
--- Preferences/TISInputSource+.swift
+++ Preferences/TISInputSource+.swift
@@ -10,30 +10,30 @@ import Carbon
 import Foundation
 
 // pod으로 설치하면 정상적으로 불러와지지 않는다
-extension TISInputSource {
-    public class func sources(withProperties properties: NSDictionary, includeAllInstalled: Bool) -> [TISInputSource]? {
+public extension TISInputSource {
+    class func sources(withProperties properties: NSDictionary, includeAllInstalled: Bool) -> [TISInputSource]? {
         guard let unmanaged = TISCreateInputSourceList(properties, includeAllInstalled) else {
             return nil
         }
         return unmanaged.takeRetainedValue() as? [TISInputSource]
     }
 
-    public func property(forKey key: String) -> Any? {
+    func property(forKey key: String) -> Any? {
         guard let unmanaged = TISGetInputSourceProperty(self, key as CFString) else {
             return nil
         }
         return Unmanaged<AnyObject>.fromOpaque(unmanaged).takeUnretainedValue()
     }
 
-    public var enabled: Bool {
+    var enabled: Bool {
         return property(forKey: kTISPropertyInputSourceIsEnabled as String) as! Bool
     }
 
-    public var identifier: String {
+    var identifier: String {
         return property(forKey: kTISPropertyInputSourceID as String) as! String
     }
 
-    public var localizedName: String {
+    var localizedName: String {
         return property(forKey: kTISPropertyLocalizedName as String) as! String
     }
 }
