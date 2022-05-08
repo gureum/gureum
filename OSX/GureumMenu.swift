0b3af66ca76d71f7740e3f0d49cb8a0961e30ad4
Chayoung You <yousbe@gmail.com>
2022-05-08 23:18:25 +0900

    Show modal when checkExperimentalVersion runs

--- OSX/GureumMenu.swift
+++ OSX/GureumMenu.swift
@@ -25,26 +25,35 @@ extension InputController {
         preferencesWindow.showWindow(nil)
     }
 
-    @IBAction func checkRecentVersion(_: Any) {
-        answers.logMenu(name: "check-version")
-        UpdateManager.shared.requestVersionInfo(mode: .Stable) { info in
+    private func checkVersion(mode: UpdateMode) {
+        var title = ""
+        var version = ""
+        switch mode {
+        case .Stable:
+            title = "업데이트"
+            version = "버전"
+        case .Experimental:
+            title = "실험 버전"
+            version = "실험 버전"
+        }
+        UpdateManager.shared.requestVersionInfo(mode: mode) { info in
             guard let info = info else {
                 let alert = NSAlert()
-                alert.messageText = "구름 입력기 업데이트 확인"
+                alert.messageText = "구름 입력기 \(title) 확인"
                 alert.addButton(withTitle: "확인")
-                alert.informativeText = "업데이트 정보에 접근할 수 없습니다. 인터넷에 연결되어 있지 않거나 구름 업데이트의 버그일 수 있습니다."
+                alert.informativeText = "\(title) 정보에 접근할 수 없습니다. 인터넷에 연결되어 있지 않거나 구름 업데이트의 버그일 수 있습니다."
                 alert.runModal()
                 return
             }
             if info.update.version == info.current {
-                let fmt = "현재 사용하고 있는 구름 입력기 \(info.current ?? "-") 는 최신 버전입니다."
+                let fmt = "현재 사용하고 있는 구름 입력기 \(info.current ?? "-") 는 최신 \(version)입니다."
                 let alert = NSAlert()
-                alert.messageText = "구름 입력기 업데이트 확인"
+                alert.messageText = "구름 입력기 \(title) 확인"
                 alert.addButton(withTitle: "확인")
                 alert.informativeText = fmt
                 alert.runModal()
             } else {
-                var message = "현재 사용하고 있는 구름 입력기는 \(info.current ?? "-") 이고 최신 버전은 \(info.update.version) 입니다. 업데이트는 로그아웃하거나 재부팅해야 적용됩니다."
+                var message = "현재 사용하고 있는 구름 입력기는 \(info.current ?? "-") 이고 최신 \(version)은 \(info.update.version) 입니다. 업데이트는 로그아웃하거나 재부팅해야 적용됩니다."
                 if !info.update.description.isEmpty {
                     message += " 업데이트 요약은 '\(info.update.description)' 입니다."
                 }
@@ -54,7 +63,7 @@ extension InputController {
                     message += " 현재 업데이트 링크를 찾을 수 없습니다. 버그 리포트를 부탁드립니다."
                 }
                 let alert = NSAlert()
-                alert.messageText = "구름 입력기 업데이트 확인"
+                alert.messageText = "구름 입력기 \(title) 확인"
                 alert.addButton(withTitle: "확인")
                 alert.informativeText = message
                 alert.runModal()
@@ -62,19 +71,14 @@ extension InputController {
         }
     }
 
+    @IBAction func checkRecentVersion(_: Any) {
+        answers.logMenu(name: "check-version")
+        checkVersion(mode: .Stable)
+    }
+
     @IBAction func checkExperimentalVersion(_: Any) {
         answers.logMenu(name: "check-experimental")
-        UpdateManager.shared.requestVersionInfo(mode: .Experimental) { info in
-            guard let info = info else {
-                let alert = NSAlert()
-                alert.messageText = "구름 입력기 실험 버전 확인"
-                alert.addButton(withTitle: "확인")
-                alert.informativeText = "현재 운영중인 실험 버전이 없습니다. 나중에 다시 확인해 주세요."
-                alert.runModal()
-                return
-            }
-            NSWorkspace.shared.open(URL(string: info.update.url)!)
-        }
+        checkVersion(mode: .Experimental)
     }
 
     @IBAction func openWebsite(_: Any) {
